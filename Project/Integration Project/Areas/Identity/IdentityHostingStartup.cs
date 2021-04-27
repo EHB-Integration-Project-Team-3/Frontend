c2febe5978c334a5f21ec96e1dd8d28df41d7d26
Michaël <michaelbracke@hotmail.com>
2021-04-27 20:35:06 +0200

    Cloning new seed

---
+++ Project/Integration Project/Areas/Identity/IdentityHostingStartup.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using Integration_Project.Areas.Identity.Data;
+using Integration_Project.Data;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Identity.UI;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+
+[assembly: HostingStartup(typeof(Integration_Project.Areas.Identity.IdentityHostingStartup))]
+namespace Integration_Project.Areas.Identity
+{
+    public class IdentityHostingStartup : IHostingStartup
+    {
+        public void Configure(IWebHostBuilder builder)
+        {
+            builder.ConfigureServices((context, services) => {
+                services.AddDbContext<Integration_ProjectContext>(options =>
+                    options.UseSqlServer(
+                        context.Configuration.GetConnectionString("DefaultConnection")));
+            });
+        }
+    }
+}
