eef80da301dae80422fcd506b4e8fae48fe9738d
steffen-dc <steffen.de.coster@student.ehb.be>
2021-05-02 17:06:43 +0200

    Added models

---
+++ Project/Integration Project/Models/Event.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Xml.Serialization;
+
+namespace Integration_Project.Models
+{
+    [XmlRoot(ElementName = "event")]
+    public class Event
+    {
+        [Key]
+        [XmlAttribute("uuid")]
+        public Guid Uuid { get; set; }
+
+        [XmlAttribute("entityVersion")]
+        public int EntityVersion { get; set; }
+
+        [XmlAttribute("title")]
+        public string Title { get; set; }
+
+        [XmlAttribute("organiserId")]
+        public Guid OrganiserId { get; set; }
+
+        [XmlAttribute("description")]
+        public string Description { get; set; }
+
+        [XmlAttribute("start")]
+        public DateTime Start { get; set; }
+
+        [XmlAttribute("end")]
+        public DateTime End { get; set; }
+
+        [XmlElement("location")]
+        public Location Location { get; set; }
+    }
+}
---
+++ Project/Integration Project/Models/Location.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Xml.Serialization;
+
+namespace Integration_Project.Models
+{
+    [Serializable]
+    public class Location
+    {
+        [Key]
+        [XmlAttribute("uuid")]
+        public Guid Uuid { get; set; }
+
+        [XmlAttribute("streetName")]
+        public string StreetName { get; set; }
+
+        [XmlAttribute("number")]
+        public string Number { get; set; }
+
+        [XmlAttribute("city")]
+        public string City { get; set; }
+
+        [XmlAttribute("postalCode")]
+        public string PostalCode { get; set; }
+    }
+}
