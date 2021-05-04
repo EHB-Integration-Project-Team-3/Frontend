609d348a53053298d6bd2acf54225643844adaf2
steffen-dc <steffen.de.coster@student.ehb.be>
2021-05-04 22:50:31 +0200

    Put RabbitMQ Receiver in a separate project

--- Project/Integration Project/Program.cs
+++ Project/Integration Project/Program.cs
@@ -17,27 +17,11 @@ namespace Integration_Project
         public static void Main(string[] args)
         {
             new Timer((e) => { Rabbit.Send<Heartbeat>(new Heartbeat(), RabbitMQ.Constants.WarningX, RabbitMQ.Constants.MonitoringHeartbeatQ); }, null, TimeSpan.Zero, TimeSpan.FromSeconds(1));
-            //Rabbit.ReceiveEvents<Event>();
 
-            //Process.Start(@"D:\GitHub\Frontend\Project\RabbitMQ Receiver\RabbitMQ Receiver.csproj");
-
-            var factory = new ConnectionFactory() { HostName = RabbitMQ.Constants.Connection, DispatchConsumersAsync = true };
-            using var connection = factory.CreateConnection();
-            using var channel = connection.CreateModel();
-            channel.QueueDeclare(queue: RabbitMQ.Constants.FrontendEventQ,
-                                 durable: false,
-                                 exclusive: false,
-                                 autoDelete: false,
-                                 arguments: null);
-
-            var consumer = new AsyncEventingBasicConsumer(channel);
-            consumer.Received += Consumer_Received;
-
-            channel.BasicConsume(queue: RabbitMQ.Constants.FrontendEventQ,
-                                         autoAck: true,
-                                         consumer: consumer);
+            var receiver = Process.Start(@"D:\GitHub\Frontend\Project\RabbitMQ Receiver\bin\Release\netcoreapp3.1\RabbitMQ Receiver"); //!!!!
 
             CreateHostBuilder(args).Build().Run();
+            receiver.Kill();
         }
 
         public static IHostBuilder CreateHostBuilder(string[] args) =>
@@ -46,17 +30,5 @@ namespace Integration_Project
                 {
                     webBuilder.UseStartup<Startup>();
                 });
-
-        private static async Task Consumer_Received(object sender, BasicDeliverEventArgs @event)
-        {
-            var message = Encoding.UTF8.GetString(@event.Body.ToArray());
-            Event receivedEvent = XmlController.DeserializeXmlString<Event>(message);
-
-            Debug.WriteLine($"Begin processing {message}");
-
-            await Task.Delay(250);
-
-            Debug.WriteLine($"End processing {receivedEvent.Title}");
-        }
     }
 }
--- Project/Integration Project/RabbitMQ/Rabbit.cs
+++ Project/Integration Project/RabbitMQ/Rabbit.cs
@@ -38,38 +38,5 @@ namespace Integration_Project.RabbitMQ
                     body: body);
             }
         }
-
-        public static void ReceiveEvents<Event>()
-        {
-            var factory = new ConnectionFactory() { HostName = Constants.Connection, DispatchConsumersAsync = true };
-            using var connection = factory.CreateConnection();
-            using var channel = connection.CreateModel();
-            channel.QueueDeclare(queue: Constants.FrontendEventQ,
-                                 durable: false,
-                                 exclusive: false,
-                                 autoDelete: false,
-                                 arguments: null);
-
-            var consumer = new AsyncEventingBasicConsumer(channel);
-            consumer.Received += Consumer_Received;
-
-            channel.BasicConsume(queue: Constants.FrontendEventQ,
-                                         autoAck: true,
-                                         consumer: consumer);
-
-            Debug.WriteLine(" Press [enter] to exit.");
-        }
-
-        private static async Task Consumer_Received(object sender, BasicDeliverEventArgs @event)
-        {
-            var message = Encoding.UTF8.GetString(@event.Body.ToArray());
-            Event receivedEvent = XmlController.DeserializeXmlString<Event>(message);
-
-            Debug.WriteLine($"Begin processing {message}");
-
-            await Task.Delay(250);
-
-            Debug.WriteLine($"End processing {receivedEvent.Title}");
-        }
     }
 }
