552c86d9d8789b963f29d4f2831bce98e6f8ac0f
Michaël <michaelbracke@hotmail.com>
2021-05-05 21:28:36 +0200

    Fix

--- Project/Integration Project/obj/Release/netcoreapp3.1/PubTmp/Out/wwwroot/js/site.js
+++ Project/Integration Project/obj/Release/netcoreapp3.1/PubTmp/Out/wwwroot/js/site.js
@@ -2,3 +2,16 @@
 // for details on configuring this project to bundle and minify static web assets.
 
 // Write your JavaScript code.
+
+
+//------- EVENT -----------//
+
+//INIT
+
+$(function () {
+    // INITIALIZE DATEPICKER PLUGIN
+    $('.datepicker').datepicker({
+        clearBtn: true,
+        format: "dd/mm/yyyy"
+    });
+});
