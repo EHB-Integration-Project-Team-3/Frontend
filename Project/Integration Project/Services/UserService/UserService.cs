8fceaa2a20ac2446ef267575d41e78ba94e0bb8f
YoniCelen <yoni.celen@hotmail.com>
2021-05-18 10:12:28 +0200

    added IUserService working on UserService and UserController

---
+++ Project/Integration Project/Services/UserService/UserService.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Integration_Project.Services.UserService
+{
+    public class UserService
+    {
+    }
+}
---
+++ Project/Integration Project/Services/UserService/interface/IUserService.cs
@@ -0,0 +1,12 @@
+﻿using Integration_Project.Areas.Identity.Data;
+using Integration_Project.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Integration_Project.Services.EventService.Interface
+{
+    public interface IUserService : IBaseService<User> {
+    }
+}
