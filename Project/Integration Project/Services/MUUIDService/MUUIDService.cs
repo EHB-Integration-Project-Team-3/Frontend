b05ec174cb3c68679ee744e9d869044b7ff97b17
steffen-dc <steffen.de.coster@student.ehb.be>
2021-05-30 21:36:29 +0200

    Added XSD validation

--- Project/Integration Project/Services/MUUIDService/MUUIDService.cs
+++ Project/Integration Project/Services/MUUIDService/MUUIDService.cs
@@ -11,16 +11,19 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 
-namespace Integration_Project.Services.MUUIDService {
-    public class MUUIDService : IMUUIDService {
-
+namespace Integration_Project.Services.MUUIDService
+{
+    public class MUUIDService : IMUUIDService
+    {
         private readonly IConfiguration _config;
 
-        public MUUIDService(IConfiguration config) {
+        public MUUIDService(IConfiguration config)
+        {
             _config = config;
         }
 
-        public void InsertIntoMUUID(MUUIDSend sendModel) {
+        public void InsertIntoMUUID(MUUIDSend sendModel)
+        {
             string constring = _config.GetConnectionString("MUUIDConnection");
             string sql =
                 $"INSERT INTO master ( UUID, Source_EntityId, EntityType, Source) VALUES(" +
@@ -28,80 +31,105 @@ namespace Integration_Project.Services.MUUIDService {
                 $"'{sendModel.Source_EntityId}'," +
                 $"'{sendModel.EntityType}'," +
                 $"'{sendModel.Source}');";
-            try {
-                using (MySqlConnection connection = new MySqlConnection(constring)) {
+            try
+            {
+                using (MySqlConnection connection = new MySqlConnection(constring))
+                {
                     connection.Open();
-                    using (MySqlCommand command = new MySqlCommand(sql, connection)) {
-                        using (MySqlDataReader reader = command.ExecuteReader()) {
-
+                    using (MySqlCommand command = new MySqlCommand(sql, connection))
+                    {
+                        using (MySqlDataReader reader = command.ExecuteReader())
+                        {
                         }
                     }
                     connection.Close();
                 }
-            } catch (Exception ex) {
+            }
+            catch (Exception ex)
+            {
                 Console.WriteLine("Error inserting model in muuid, please check connection or service");
             }
         }
 
-        public MUUIDReceive Get(Guid uuid) {
+        public MUUIDReceive Get(Guid uuid)
+        {
             string constring = _config.GetConnectionString("MUUIDConnection");
             string sql = $"SELECT * FROM master WHERE UUID = UUID_TO_BIN('{uuid}');";
             MUUIDReceive receivedModal = new MUUIDReceive();
-            try {
-                using (MySqlConnection connection = new MySqlConnection(constring)) {
+            try
+            {
+                using (MySqlConnection connection = new MySqlConnection(constring))
+                {
                     connection.Open();
-                    using (MySqlCommand command = new MySqlCommand(sql, connection)) {
-                        using (MySqlDataReader reader = command.ExecuteReader()) {
-                            while (reader.Read()) {
-                                receivedModal.Uuid = new Guid((byte[]) reader.GetValue(1));
-                                receivedModal.Source_EntityId = (string) reader.GetValue(2);
-                                receivedModal.EntityType = (string) reader.GetValue(3);
-                                receivedModal.EntityVersion = (int) reader.GetValue(4);
-                                receivedModal.Source = (string) reader.GetValue(5);
+                    using (MySqlCommand command = new MySqlCommand(sql, connection))
+                    {
+                        using (MySqlDataReader reader = command.ExecuteReader())
+                        {
+                            while (reader.Read())
+                            {
+                                receivedModal.Uuid = new Guid((byte[])reader.GetValue(1));
+                                receivedModal.Source_EntityId = (string)reader.GetValue(2);
+                                receivedModal.EntityType = (string)reader.GetValue(3);
+                                receivedModal.EntityVersion = (int)reader.GetValue(4);
+                                receivedModal.Source = (string)reader.GetValue(5);
                             }
                         }
                     }
                     connection.Close();
                     return receivedModal;
                 }
-            } catch (Exception ex) {
+            }
+            catch (Exception ex)
+            {
                 Console.WriteLine("Error retrieving muuid, please check connection or service");
                 return null;
             }
         }
 
-        public bool UpdateEntityVersion(MUUIDSend sendModal, int currentEntityVersion) {
+        public bool UpdateEntityVersion(MUUIDSend sendModal, int currentEntityVersion)
+        {
             string constring = _config.GetConnectionString("MUUIDConnection");
             string sql = $"update master set EntityVersion = {currentEntityVersion} " +
                 $"where Source = '{sendModal.Source}' and EntityVersion = {currentEntityVersion} - 1 and UUID = UUID_TO_BIN('{sendModal.Uuid}') and " +
                 $"(select EntityVersion from master where Source = 'Canvas') < {currentEntityVersion} and " +
                 $"(select EntityVersion from master where Source = 'PLANNING') < {currentEntityVersion};";
-            try {
-                using (MySqlConnection connection = new MySqlConnection(constring)) {
+            try
+            {
+                using (MySqlConnection connection = new MySqlConnection(constring))
+                {
                     connection.Open();
-                    using (MySqlCommand command = new MySqlCommand(sql, connection)) {
+                    using (MySqlCommand command = new MySqlCommand(sql, connection))
+                    {
                         command.ExecuteReader();
                     }
                     connection.Close();
                     return true;
                 }
-            } catch (Exception ex) {
+            }
+            catch (Exception ex)
+            {
                 Console.WriteLine("Error updating model in muuid database");
                 return false;
             }
         }
 
-        public Guid GetUUID() {
+        public Guid GetUUID()
+        {
             /// TODO: Nog extra toevoegen dat backup conn.string ook wordt toegepast
             string constring = _config.GetConnectionString("MUUIDConnection");
             string sql = "SELECT UUID()";
             string uuid = "";
-            try {
-                using (MySqlConnection connection = new MySqlConnection(constring)) {
+            try
+            {
+                using (MySqlConnection connection = new MySqlConnection(constring))
+                {
                     connection.Open();
-                    using (MySqlCommand command = new MySqlCommand(sql, connection)) {
-                        using (MySqlDataReader reader = command.ExecuteReader()) {
-                            while (reader.Read()) {
+                    using (MySqlCommand command = new MySqlCommand(sql, connection))
+                    {
+                        using (MySqlDataReader reader = command.ExecuteReader())
+                        {
+                            while (reader.Read())
+                            {
                                 uuid = reader.GetString(0);
                             }
                         }
@@ -109,7 +137,9 @@ namespace Integration_Project.Services.MUUIDService {
                     connection.Close();
                     return Guid.Parse(uuid);
                 }
-            } catch (Exception ex) {
+            }
+            catch (Exception ex)
+            {
                 Console.WriteLine("Error retrieving muuid, please check connection or service");
                 return Guid.Empty;
             }
--- Project/RabbitMQ Receiver/Program.cs
+++ Project/RabbitMQ Receiver/Program.cs
@@ -12,12 +12,14 @@ using System.Threading;
 using Integration_Project.Services.MUUIDService;
 using Integration_Project.Services.MUUIDService.Interface;
 
-namespace RabbitMQ_Receiver {
-    public class Program {
+namespace RabbitMQ_Receiver
+{
+    public class Program
+    {
         private static readonly IMUUIDService _muuidService;
 
-
-        public static void Main(string[] args) {
+        public static void Main(string[] args)
+        {
             //Heartbeat
             new Timer((e) => { Rabbit.Send<Heartbeat>(new Heartbeat(), "", Integration_Project.RabbitMQ.Constants.MonitoringHeartbeatQ); }, null, TimeSpan.Zero, TimeSpan.FromSeconds(1));
 
@@ -43,74 +45,113 @@ namespace RabbitMQ_Receiver {
             Console.ReadLine();
         }
 
-        private static async Task Consumer_Received(object sender, BasicDeliverEventArgs @event) {
-            try {
+        private static async Task Consumer_Received(object sender, BasicDeliverEventArgs @event)
+        {
+            try
+            {
                 var message = Encoding.UTF8.GetString(@event.Body.ToArray());
 
-                if (@event.Exchange == Integration_Project.RabbitMQ.Constants.EventX) {
+                if (@event.Exchange == Integration_Project.RabbitMQ.Constants.EventX)
+                {
                     Console.WriteLine($"\nReceived Event:\n{message}");
 
-                    Event receivedEvent = XmlController.DeserializeXmlString<Event>(message);
+                    if (XmlController.ValidateXml(message, typeof(Event)))
+                    {
+                        Event receivedEvent = XmlController.DeserializeXmlString<Event>(message);
 
-                    if (receivedEvent != default)
-                        Console.WriteLine($"\nEvent succesfully deserialized");
+                        if (receivedEvent != default)
+                            Console.WriteLine($"\nEvent succesfully deserialized");
 
-                    receivedEvent.Location = Location.FromRabbitMQ(receivedEvent.LocationRabbit);
+                        receivedEvent.Location = Location.FromRabbitMQ(receivedEvent.LocationRabbit);
 
-                    try {
-                        if (receivedEvent.Header.Method == Method.CREATE) {
-                            if (new EventService().Add(receivedEvent))
-                                Console.WriteLine($"\nEvent successfully added to database");
-                            else
-                                Console.WriteLine($"\nFailed to add event to database");
-                        } else if (receivedEvent.Header.Method == Method.UPDATE) {
-                            if (new EventService().Update(receivedEvent))
-                                Console.WriteLine($"\nEvent successfully updated in database");
-                            else
-                                Console.WriteLine($"\nFailed to update event in database");
-                        } else if (receivedEvent.Header.Method == Method.DELETE) {
-                            if (new EventService().Delete(receivedEvent.Uuid))
-                                Console.WriteLine($"\nEvent successfully deleted from database");
-                            else
-                                Console.WriteLine($"\nFailed to delete event from database");
+                        try
+                        {
+                            if (receivedEvent.Header.Method == Method.CREATE)
+                            {
+                                if (new EventService().Add(receivedEvent))
+                                    Console.WriteLine($"\nEvent successfully added to database");
+                                else
+                                    Console.WriteLine($"\nFailed to add event to database");
+                            }
+                            else if (receivedEvent.Header.Method == Method.UPDATE)
+                            {
+                                if (receivedEvent.EntityVersion > _muuidService.Get(receivedEvent.Uuid).EntityVersion)
+                                    if (new EventService().Update(receivedEvent))
+                                        Console.WriteLine($"\nEvent successfully updated in database");
+                                    else
+                                        Console.WriteLine($"\nFailed to update event in database");
+                                else
+                                    Console.WriteLine($"\nReceived event is outdated");
+                            }
+                            else if (receivedEvent.Header.Method == Method.DELETE)
+                            {
+                                if (new EventService().Delete(receivedEvent.Uuid))
+                                    Console.WriteLine($"\nEvent successfully deleted from database");
+                                else
+                                    Console.WriteLine($"\nFailed to delete event from database");
+                            }
                         }
-                    } catch (Exception ex) {
-                        Console.WriteLine(ex);
+                        catch (Exception ex)
+                        {
+                            Console.WriteLine(ex);
+                        }
                     }
-                } else if (@event.Exchange == Integration_Project.RabbitMQ.Constants.UserX) {
+                }
+                else if (@event.Exchange == Integration_Project.RabbitMQ.Constants.UserX)
+                {
                     Console.WriteLine($"\nReceived User:\n{message}");
 
-                    InternalUser receivedUser = XmlController.DeserializeXmlString<InternalUser>(message);
+                    if (XmlController.ValidateXml(message, typeof(InternalUser)))
+                    {
+                        InternalUser receivedUser = XmlController.DeserializeXmlString<InternalUser>(message);
 
-                    if (receivedUser != default)
-                        Console.WriteLine($"\nUser succesfully deserialized");
+                        if (receivedUser != default)
+                            Console.WriteLine($"\nUser successfully deserialized");
 
-                    try {
-                        if (receivedUser.Header.Method == Method.CREATE) {
-                            if (new UserService().Add(receivedUser))
-                                Console.WriteLine($"\nUser successfully added to database");
-                            else
-                                Console.WriteLine($"\nFailed to add user to database");
-                        } else if (receivedUser.Header.Method == Method.UPDATE) {
-                            if (new UserService().Update(receivedUser))
-                                Console.WriteLine($"\nUser successfully updated in database");
-                            else
-                                Console.WriteLine($"\nFailed to update user in database");
-                        } else if (receivedUser.Header.Method == Method.DELETE) {
-                            //if (new UserService().Delete(receivedUser.Uuid))
-                            //    Console.WriteLine($"\nUser successfully deleted from database");
-                            //else
-                            //    Console.WriteLine($"\nFailed to delete user from database");
+                        try
+                        {
+                            if (receivedUser.Header.Method == Method.CREATE)
+                            {
+                                if (new UserService().Add(receivedUser))
+                                    Console.WriteLine($"\nUser successfully added to database");
+                                else
+                                    Console.WriteLine($"\nFailed to add user to database");
+                            }
+                            else if (receivedUser.Header.Method == Method.UPDATE)
+                            {
+                                if (receivedUser.EntityVersion > _muuidService.Get(receivedUser.Uuid).EntityVersion)
+                                    if (new UserService().Update(receivedUser))
+                                        Console.WriteLine($"\nUser successfully updated in database");
+                                    else
+                                        Console.WriteLine($"\nFailed to update user in database");
+                                else
+                                    Console.WriteLine($"\nReceived user is outdated");
+                            }
+                            else if (receivedUser.Header.Method == Method.DELETE)
+                            {
+                                //if (new UserService().Delete(receivedUser.Uuid))
+                                //    Console.WriteLine($"\nUser successfully deleted from database");
+                                //else
+                                //    Console.WriteLine($"\nFailed to delete user from database");
+                            }
                         }
-                    } catch (Exception ex) {
-                        Console.WriteLine(ex);
+                        catch (Exception ex)
+                        {
+                            Console.WriteLine(ex);
+                        }
                     }
-                } else if (@event.Exchange == Integration_Project.RabbitMQ.Constants.AttendanceX) {
+                }
+                else if (@event.Exchange == Integration_Project.RabbitMQ.Constants.AttendanceX)
+                {
                     //moet frontend attendance kunnen ontvangen????
                 }
-            } catch (Exception ex) {
+            }
+            catch (Exception ex)
+            {
                 Console.WriteLine(ex);
-            } finally {
+            }
+            finally
+            {
                 Console.WriteLine("\nWaiting for message...");
                 await Task.Delay(1000);
             }
