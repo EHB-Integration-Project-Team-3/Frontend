5bee53203ca5a3ab5f6afe5c57e5b814f5912edf
YoniCelen <yoni.celen@hotmail.com>
2021-06-01 02:08:45 +0200

    added IAttendanceService

---
+++ Project/Integration Project/Services/AttendanceService/AttendanceService.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Integration_Project.Services.AttendanceService
+{
+    public class AttendanceService : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
